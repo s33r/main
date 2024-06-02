ba24495ec6a98e1e80adc7ac808efc2f51015d74
Aaron Willows <aaron@aaronwillows.com>
2024-06-02 01:42:07 +0000

    Started to build out repo infra

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # main
 The main repository
+
+## Organization
+
+| Directory.      | Package Prefix.     | Purpose 
+|-----------------|---------------------|-----------
+| packages/app.   | @ehwillows/app.*.   | Contains application packages
+| packages/lib    | @ehwillows/lib.*    | Contains reusable packages for apps or others.
+| packages/wright | @ehwillows/wright.* | Contains automation for building, validating and bundling packages.
+
+## Scripts
+
+| Command | Description
+| npx cli bundle | _Not Implemented Yet_ Bundles an application or library for distribution
+| npx cli serve  | _Not Implemented Yet_ Serves an application 
+| npx cli lint.  | _Not Implemented Yet_ Validates the code of a package
+| npx cli deploy | _Not Implemented Yet_ Deploys a package to a target
+
+
---
+++ eslint.config.mjs
@@ -0,0 +1,90 @@
+import alignAssignments from 'eslint-plugin-align-assignments';
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import typescriptEslint from 'typescript-eslint';
+
+export default [
+    eslint.configs.recommended,
+    ...typescriptEslint.configs.strict,
+    {
+        plugins: {
+            '@stylistic'       : stylistic,
+            'align-assignments': alignAssignments,
+        },
+        languageOptions: {
+            parser       : typescriptEslint.parser,
+            parserOptions: {
+                project        : true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            '@typescript-eslint/prefer-literal-enum-member': 'off',
+
+            'unicode-bom'          : ['error', 'never'],
+            'one-var'              : ['error', 'never'],
+            'no-var'               : 'error',
+            'no-eval'              : 'error',
+            'no-new-func'          : 'error',
+            'no-implied-eval'      : 'error',
+            'no-alert'             : 'error',
+            'no-console'           : 'warn',
+            'require-await'        : 'warn',
+            'new-cap'              : 'error',
+            'func-style'           : ['error', 'expression'],
+            camelcase              : ['error', { ignoreImports: true }],
+            'no-constructor-return': 'error',
+            'no-unreachable'       : 'error',
+            'no-await-in-loop'     : 'warn',
+            curly                  : ['error', 'multi-line'],
+            'no-restricted-imports': ['error', {
+                patterns: [
+                    {
+                        group  : ['.*'],
+                        message: 'All imports must be from an npm package.. Hint: all packages start with @ehwillows/',
+                    },
+                ],
+            }],
+
+            '@typescript-eslint/no-explicit-any'                : 'warn',
+            '@typescript-eslint/no-unused-vars'                 : 'warn',
+            '@typescript-eslint/consistent-generic-constructors': 'error',
+            '@typescript-eslint/consistent-indexed-object-style': 'error',
+            '@stylistic/type-generic-spacing'                   : 'error',
+            '@stylistic/semi'                                   : ['error', 'always'],
+            '@stylistic/semi-style'                             : ['error', 'last'],
+            '@stylistic/semi-spacing'                           : 'error',
+            '@stylistic/quotes'                                 : ['error', 'single'],
+            '@stylistic/comma-dangle'                           : ['error', 'always-multiline'],
+            '@stylistic/comma-spacing'                          : ['error', {
+                before: false,
+                after : true,
+            }],
+            '@stylistic/key-spacing'            : ['error', { align: 'colon' }],
+            '@stylistic/quote-props'            : ['error', 'as-needed'],
+            '@stylistic/eol-last'               : ['error', 'never'],
+            '@stylistic/linebreak-style'        : ['error', 'unix'],
+            '@stylistic/brace-style'            : ['error', '1tbs', { allowSingleLine: true } ],
+            '@stylistic/indent'                 : 'error',
+            '@stylistic/no-trailing-spaces'     : 'error',
+            '@stylistic/space-in-parens'        : ['error', 'never'],
+            '@stylistic/dot-location'           : ['error', 'property'],
+            '@stylistic/function-call-spacing'  : ['error', 'never'],
+            '@stylistic/object-property-newline': 'error',
+            '@stylistic/object-curly-newline'   : ['error', {
+                minProperties: 3,
+                multiline    : true,
+                consistent   : true,
+            }],
+            'space-before-function-paren': ['error', {
+                anonymous : 'always',
+                named     : 'never',
+                asyncArrow: 'always',
+            }],
+            '@stylistic/space-infix-ops'     : 'error',
+            '@stylistic/object-curly-spacing': ['error', 'always'],
+
+            'align-assignments/align-assignments': 'error',
+        },
+    },
+];
